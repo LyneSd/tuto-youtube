b2f52c0cd596f8303c759af01f5872269149eaf2
LyneSd <67509085+LyneSd@users.noreply.github.com>
2022-08-17 10:16:30 +0000

    deleted poetry

--- scraper.py
+++ scraper.py
@@ -1,22 +1,13 @@
-#beutiful soup clearify the structure of html code + access some info
-#replit installs libraries automatically
-
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+#library for chrome options qui sont indisponsable pour replit for some reason? 
+ 
 
 SiteENSH = 'https://www.ensh.dz/category/marches-publiques/'
-HtmlText= requests.get(SiteENSH)
-#status code => indicates if the import of request was successful
-print ('status code' , HtmlText.status_code)
-#HtmlText.text contains all html code in website
-#print ('output', HtmlText.text[:1000])
-#save file as HTML to inspect it later
-with open('ENSH.html', 'w') as f:
-  f.write(HtmlText.text)
-#import beutiful soup to structure code better (plus lisible)
-soup = BeautifulSoup(HtmlText.text, 'html.parser')
 
-print('Page title:', soup.title.text)
 
-#since request only excutes the html text in HtmlText= requests.get(SiteENSH) and doesnt excute the JS code cause it doesnt contain a browser we should connect it to one simulate a headless fakebrowser to run JS on the page , to do that use selinum: selinum automates browsers 
-#request comes with chrome and chrome driver preinstalled 
+
+#driver is a simulation of a driver in request that we can use , so we are loading the page HtmlText in it
+driver = webdriver.Chrome()
+driver.get('https://www.repl.it/')
+print(driver.title)
+
